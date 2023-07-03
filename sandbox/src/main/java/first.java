88d6c0d2bdee2fdc98fb624a0c264edaa8935c3b
BobcatJo <130566127+BobcatJo@users.noreply.github.com>
2023-07-03 18:22:53 +0300

    v4

--- sandbox/src/main/java/first.java
+++ sandbox/src/main/java/first.java
@@ -1,7 +1,7 @@
 public class first {
 
-	public static void main(String[] args) {
-		System.out.println("Hi, boss!!!");
-}
+  public static void main(String[] args) {
+    System.out.println("Hi, boss!!!");
+  }
 
 }
